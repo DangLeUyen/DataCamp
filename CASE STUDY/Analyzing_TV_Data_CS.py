bdb471c5e43d689b17c029dda1aad6e650b39e53
UyenDang <dtleuyen@gmail.com>
2020-12-21 19:38:55 +0700

    Create Analyzing_TV_Data_CS.py

---
+++ CASE STUDY/Analyzing_TV_Data_CS.py
@@ -0,0 +1,44 @@
+# Import pandas
+import pandas as pd
+
+# Load the CSV data into DataFrames
+super_bowls = pd.read_csv('datasets/super_bowls.csv')
+tv = pd.read_csv('datasets/tv.csv')
+halftime_musicians = pd.read_csv('datasets/halftime_musicians.csv')
+
+# Display the first five rows of each DataFrame
+display(super_bowls.head())
+display(tv.head())
+display(halftime_musicians.head())
+
+
+
+# 2. Taking note of dataset issues¶
+# Summary of the TV data to inspect
+tv.info()
+
+print('\n')
+
+# Summary of the halftime musician data to inspect
+halftime_musicians.info()
+
+
+
+# 3. Combined points distribution¶
+# Import matplotlib and set plotting style
+from matplotlib import pyplot as plt
+%matplotlib inline
+plt.style.use('seaborn')
+
+# Plot a histogram of combined points
+plt.hist(super_bowls.combined_pts)
+plt.xlabel('Combined Points')
+plt.ylabel('Number of Super Bowls')
+plt.show()
+
+# Display the Super Bowls with the highest and lowest combined scores
+display(super_bowls[super_bowls['combined_pts'] > 70])
+display(super_bowls[super_bowls['combined_pts'] < 25])
+
+
+# 4. Point difference distribution¶
